87e8c7fd57670d8c02b04b50affaea3847d09f8f
jmechristian <jmechristian@gmail.com>
2024-05-27 14:20:52 -0400

    page title

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -8,12 +8,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Interiors | Akseizer Design Group</title>
+        <title>Branding | Akseizer Design Group</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='title' content='Interiors | Akseizer Design Group' />
+        <meta name='title' content='Branding | Akseizer Design Group' />
         <meta
           name='description'
-          content='Creating heartfelt interiors that inspire and connect.'
+          content='Transforming brands with innovative marketing strategies and design.'
         />
         <meta
           name='keywords'
